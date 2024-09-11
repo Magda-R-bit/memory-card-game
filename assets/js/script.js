7aff454677ff87e7b42ba708896bc2fd4cb56bda
Magdalena Redka <magda.redka@gmail.com>
2024-09-11 02:04:53 +0000

    Load and generate cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,51 @@
+const gameBox = document.querySelector(".game-box");
+let cards = [];
+
+
+
+
 function loadCards() {
+    fetch("./data/cards.json")
+    .then((res) => res.json())
+    .then((data) => {
+        cards = [...data,...data];
+        //shuffleCards();
+        generateCards();
+    })
+
+    .catch(error => {
+        console.error("Error fetching the cards:", error)
+    });
 
 }
 
+loadCards()
+
 function shuffleCards() {
+    /*let currentIndex = cards.length;
+    while (currentIndex !== 0) {
+        const randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [cards[currentIndex], cards[randomIndex]] = [cards[randomIndex], cards[currentIndex]];
+    }*/
 
 }
 
 function generateCards() {
+    for (let card of cards) {
+        const cardElement = document.createElement("div");
+        cardElement.classList.add("card");
+        cardElement.setAttribute("data-name", card.name);
+        cardElement.innerHTML =`
+        <div class="front">
+        <img class ="front-image" src=${card.image} />
+        </div>
+        <div class="back"></div>
+        `;
+        gameBox.appendChild(cardElement);
+        
+    }
 
 }
 
@@ -59,5 +98,5 @@ function resetBoard() {
 }
 
 function restart() {
-    
+
 }
