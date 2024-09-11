c8de6cb63468aba626a346e01065d65d1069bd7e
Magdalena Redka <magda.redka@gmail.com>
2024-09-11 14:23:49 +0000

    Implement matching logic for card pairs.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,12 @@ let firstCard, secondCard;
 let lockBoard = false;
 
 
-
+/**
+ * Loads card data from cards.json file, duplicates it to create pairs, and prepares the game.
+ * 
+ * This function fetches the card data from ".data/cards.json", creates pairs by duplicating the fetched data, shuffles the cards, and generates them for use in the game.
+ * If an error occurs during the fetch operation, an error message is logged to the consol.
+ */
 
 function loadCards() {
     fetch("./data/cards.json")
@@ -34,6 +39,9 @@ function shuffleCards() {
 
 }
 
+/**
+ * Dynamically loops over the cards array and generates a card element for each card, and adds cards to the game box.
+ */
 function generateCards() {
     for (let card of cards) {
         const cardElement = document.createElement("div");
@@ -45,7 +53,10 @@ function generateCards() {
         </div>
         <div class="back"></div>
         `;
+        // Append the card element to the game box
         gameBox.appendChild(cardElement);
+
+        // Add event listener to handle the flip action when clicked
         cardElement.addEventListener("click", flipCard);
         
     }
@@ -63,7 +74,10 @@ function flipCard() {
         return;
     }
 
+    secondCard = this;
     lockBoard = true;
+
+    checkForMatch();
 
 }
 
@@ -83,15 +97,33 @@ function resetMoves() {
 
 }
 
+/**
+ * Checks if the two selected cards are a match by comparing their names.
+ * 
+ * If the cards match, it disables further flipping by removing the click event listeners using the `disableCards` function.
+ * If they do not match, the cards are unflipped.
+ */
 function checkForMatch() {
+    let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+    isMatch ? disableCards() : unflipCards();
 
 }
 
 function disableCards() {
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+
+    resetBoard();
 
 }
 
 function unflipCards() {
+    setTimeout(() => {
+        firstCard.classList.remove("flipped");
+        secondCard.classList.remove("flipped");
+        resetBoard();
+    }, 1000);
 
 }
 
@@ -108,6 +140,9 @@ function gameOver() {
 }
 
 function resetBoard() {
+    firstCard = null;
+    secondCard = null;
+    lockBoard = false;
 
 }
 
