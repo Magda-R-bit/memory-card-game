552a460e321beac8b4b9f69e02c70b8fec9d209f
Magdalena Redka <magda.redka@gmail.com>
2024-09-11 00:43:31 +0000

    Add js basic structure

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+function loadCards() {
+
+}
+
+function shuffleCards() {
+
+}
+
+function generateCards() {
+
+}
+
+function flipCards() {
+
+}
+
+function startTimer() {
+
+}
+
+function stopTimer() {
+
+}
+
+function resetTimer() {
+
+}
+
+function resetMoves() {
+
+}
+
+function checkForMatch() {
+
+}
+
+function disableCards() {
+
+}
+
+function unflipCards() {
+
+}
+
+function checkWinCondition() {
+
+}
+
+function showWinnerIcon() {
+
+}
+
+function gameOver() {
+
+}
+
+function resetBoard() {
+
+}
+
+function restart() {
+    
+}
